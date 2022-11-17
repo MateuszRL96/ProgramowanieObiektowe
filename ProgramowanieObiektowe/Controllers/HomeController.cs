b92bb47992c805eb803747c1ab436d88e20e9b2c
Mateusz  Kulec <w64875@student.wsiz.edu.pl>
2022-11-17 13:11:43 +0100

    zajecia 17-11.2022

---
+++ ProgramowanieObiektowe/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgramowanieObiektowe.Models;
+using System.Diagnostics;
+
+namespace ProgramowanieObiektowe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Nowa()
+        {
+            _logger.LogInformation("gladge");
+            return View(Nowa);
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProgramowanieObiektowe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgramowanieObiektowe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
